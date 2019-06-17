a2880879f1e144b7746a7557281c53f5f7860348
Juice Lizard <31448707+JuiceLizard@users.noreply.github.com>
2019-06-17 20:58:58 +0200

    Update inputs.ino

--- code/SquareNoseColor/inputs.ino
+++ code/SquareNoseColor/inputs.ino
@@ -18,34 +18,105 @@ void inputs() {
 
 // Square Nose crouches
   if(SquareNoseIsJumping == false) {
-    if(gb.buttons.repeat(BUTTON_DOWN, 0)) {
+    if(gb.buttons.repeat(BUTTON_DOWN, 0) && (electricShock == false)) {
       SquareNoseY = 30;
     } else {
       SquareNoseY = 28;
     }
   }
 
-// move Square Nose right and left
-  if(handGoesDown == true) {
-    if((gb.buttons.repeat(BUTTON_RIGHT, 0)) && (SquareNoseX < (gb.display.width() - 10))) {
-      SquareNoseX += 3;
-      if(gb.buttons.repeat(BUTTON_DOWN, 0)) {
-        SquareNoseX += 1;
-      }
-      SquareNoseGoesRight = true;
-    } else {
-      if((gb.buttons.repeat(BUTTON_LEFT, 0)) && (SquareNoseX > 0)) {
-        SquareNoseX -= 3;
-        if(gb.buttons.repeat(BUTTON_DOWN, 0)) {
-          SquareNoseX -= 1;
+// move Lethal Mouse or Square Nose right and left
+  if(playLethalMouse == true) {
+    // Lethal Mouse: when pressing RIGHT or LEFT buttons
+    if(handGoesDown == true) {
+      if(SquareNoseIsJumping == false) {
+        if(gb.buttons.repeat(BUTTON_RIGHT, 0) && (electricShock == false)) {
+          SquareNoseGoesRight = true;
+          if(SquareNoseX < (gb.display.width() - 10)) {
+            LethalMouseMoves = true;
+          }
+        } else {
+          if(gb.buttons.repeat(BUTTON_LEFT, 0) && (electricShock == false)) {
+            SquareNoseGoesRight = false;
+            if(SquareNoseX > 0) {
+              LethalMouseMoves = true;
+            }
+          }
         }
-        SquareNoseGoesRight = false;
+      }
+
+// Lethal Mouse stops or changes direction (if not in the air)
+    if(SquareNoseIsJumping == false) {
+      if(gb.buttons.repeat(BUTTON_DOWN, 0) && (electricShock == false)) {
+        LethalMouseMoves = false;
+      }
+    }
+
+// moves Lethal Mouse right and left
+      if(LethalMouseMoves == true) {
+        if((electricShock == true) && ((madLethalMouse % 5) == 0) && (madLethalMouse != 0) && (SquareNoseIsJumping == false)) {
+          if(SquareNoseGoesRight == true) {
+            SquareNoseGoesRight = false;
+          } else {
+            SquareNoseGoesRight = true;
+          }
+        }
+        if(SquareNoseGoesRight == true) {
+          SquareNoseX += 3;
+        } else {
+          SquareNoseX -= 3;
+        }
+      }
+    }
+
+    if((electricShock == true) && (SquareNoseIsJumping == false)) {
+      madLethalMouse = madLethalMouse + 1;
+      if(madLethalMouse > 5) {
+        madLethalMouse = 0;
+      }
+      LethalMouseMoves = true;
+    }
+
+// Lethal Mouse: stop when hitting a wall
+    if((SquareNoseX < 1) || (SquareNoseX > 69)) {
+      if(electricShock == false) {
+        LethalMouseMoves = false;
+      } else {
+        if(SquareNoseIsJumping == false) {
+          madLethalMouse = 0;
+          if(SquareNoseGoesRight == true) {
+            SquareNoseGoesRight = false;
+          } else {
+            SquareNoseGoesRight = true;
+          }
+          LethalMouseMoves = true;
+        }
+      }
+    }
+
+// Square Nose moves left and right
+  } else {
+    if(handGoesDown == true) {
+      if((gb.buttons.repeat(BUTTON_RIGHT, 0)) && (SquareNoseX < (gb.display.width() - 10))) {
+        SquareNoseX += 3;
+        if(gb.buttons.repeat(BUTTON_DOWN, 0)) {
+          SquareNoseX += 1;
+        }
+        SquareNoseGoesRight = true;
+      } else {
+        if((gb.buttons.repeat(BUTTON_LEFT, 0)) && (SquareNoseX > 0)) {
+          SquareNoseX -= 3;
+          if(gb.buttons.repeat(BUTTON_DOWN, 0)) {
+            SquareNoseX -= 1;
+          }
+          SquareNoseGoesRight = false;
+        }
       }
     }
   }
   
 // Square Nose jump
-  if((SquareNoseY == 28) && (gb.buttons.pressed(BUTTON_B))) {
+  if((SquareNoseY == 28) && (gb.buttons.pressed(BUTTON_B)) && (electricShock == false)) {
     SquareNoseIsJumping = true;
     SquareNoseJumpSpeed = -6;
     if((electricShock == false) && (handGoesDown == true)) {
